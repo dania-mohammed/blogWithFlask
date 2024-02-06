f96150941af9240ca0d99b286d4e5ee0dfc3c138
Dania <daniamohammedelhassan@gmail.com>
2024-02-06 10:47:02 +0200

    get started by creating home page and about page

---
+++ flaskblog.py
@@ -0,0 +1,31 @@
+from flask import Flask , render_template
+app = Flask(__name__)
+
+
+posts = [
+    {
+        'author':' Dania mohammedelhassan',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'December 19, 2019'
+    },
+     {
+        'author':' Mosab',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'December 19, 2019'
+    }
+]
+
+
+@app.route("/")
+@app.route("/home")
+def hello():
+    return render_template('home.html', posts = posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
