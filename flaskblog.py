c255d3dcdb7174a85d8e9979c2a7da30c23760bf
Dania <daniamohammedelhassan@gmail.com>
2024-02-06 16:36:00 +0200

    create the login and logout form

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,8 @@
-from flask import Flask , render_template , url_for
+from flask import Flask , render_template , url_for , flash , redirect
+from forms import RegistrationForm , loginForm
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '87d03da2b8bd2a4eab7529c38ab5b83e'
 
 posts = [
     {
@@ -27,5 +29,24 @@ def hello():
 def about():
     return render_template('about.html', title = 'about')
 
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register' , form=form)
+
+@app.route("/login",methods=['GET','POST'])
+def login():
+    form = loginForm()
+    if form.validate_on_submit():
+        if form.email.data =='admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in !', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title = 'Login' , form=form)
 if __name__ == '__main__':
     app.run(debug=True)
